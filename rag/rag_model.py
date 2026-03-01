cf05d7bf35fc56e99616d9be512e3bc60431014b
snehaaa-16 <129296722+snehaaa-16@users.noreply.github.com>
2026-03-01 17:10:16 +0530

    added sequence embedding in forward method
    Removed commented sections for clarity in the forward method.

--- rag/rag_model.py
+++ rag/rag_model.py
@@ -20,11 +20,6 @@ class RAGSequentialRec(nn.Module):
     def forward(self, sequence_ids):
 
         device = sequence_ids.device
-
-        # ---------------------------------------
-        # 1️⃣ Convert item IDs → embeddings (vectorized)
-        # ---------------------------------------
-
         # Padding assumed as 0
         # Movie IDs assumed starting from 1
         seq_ids = sequence_ids.clone()
@@ -34,17 +29,9 @@ class RAGSequentialRec(nn.Module):
 
         sequence_embeddings = self.item_embeddings[seq_ids]
         sequence_embeddings[mask] = 0.0
-
-        # ---------------------------------------
-        # 2️⃣ Baseline user representation
-        # ---------------------------------------
-
+        
         user_rep = self.base_model.rec_llm(sequence_embeddings)
-
-        # ---------------------------------------
-        # 3️⃣ Retrieval
-        # ---------------------------------------
-
+        
         indices = self.retriever.retrieve(user_rep)
         indices = indices.to(device)
 
@@ -54,18 +41,10 @@ class RAGSequentialRec(nn.Module):
         # Aggregate retrieved embeddings (mean over K)
         retrieved_embeds = retrieved_embeds.mean(dim=1)
 
-        # ---------------------------------------
-        # 4️⃣ Gated Fusion
-        # ---------------------------------------
-
         concat = torch.cat([user_rep, retrieved_embeds], dim=1)
         gate = self.sigmoid(self.gate_layer(concat))
 
         fused_rep = gate * user_rep + (1 - gate) * retrieved_embeds
-
-        # ---------------------------------------
-        # 5️⃣ Final Ranking
-        # ---------------------------------------
 
         logits = self.base_model.projection_head(fused_rep)
 
